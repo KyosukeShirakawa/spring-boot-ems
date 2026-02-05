6d68e2a1bb82ce5d711574b55bdc66a3515514cd
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-05 21:43:24 +0100

    creating employee entity

--- src/main/java/net/javaguides/ems/entity/Employee.java
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -18,7 +18,13 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name="last_name")
     private String lastName;
+
+    @Column(name="email_id", nullable = false, unique = true)
     private String email;
 }
